de5877ea124a3525eb726e8055b48621787e34bc
sracabanahashas <wkocsis@gmail.com>
2024-09-09 10:30:56 -0700

    Create ToDoController function containing ToDoItem class in ToDoFunctions.js

--- src/ToDoFunctions.js
+++ src/ToDoFunctions.js
@@ -1,1 +1,15 @@
 export const test = "Sup";
+
+export function ToDoController() {
+
+    const ToDoItem = class {
+        constructor(title, description, dueDate, priority) {
+            this.title = title;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.priority = priority;
+        }
+
+    } 
+
+}
