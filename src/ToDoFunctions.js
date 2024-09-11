806261a6c6325aa6258946356fe4e8e6e6a62e50
sracabanahashas <wkocsis@gmail.com>
2024-09-11 10:15:06 -0700

    Begin building out decoupled logic in ToDoController and domController functions, begin adding functions for creating, getting, adding and rendering todo items

--- src/ToDoFunctions.js
+++ src/ToDoFunctions.js
@@ -1,16 +1,43 @@
 export const test = "Sup";
 
+//functions: create, edit, delete todo items; create, read, edit projects
 export const ToDoController = (function () {
 
-    function createToDoItem(title, description, dueDate, priority) {
+    let toDoItem;
+    
+    function getToDoItem() {
+        return toDoItem;
+    }
+
+    function createToDoItem(title, description, dueDate, priority, project) {
         return {
             title,
             description,
             dueDate,
-            priority
+            priority,
+            project
         }
     } 
 
+    function addToDoItem() {
+        toDoItem = createToDoItem()
+    }
 
+
+
+    return {
+        createToDoItem,
+        getToDoItem,
+        addToDoItem,
+    }
+
+})();
+
+export const domController = (function () {
+
+    function renderToDoItem(todo) {
+
+        let toDoItem = 
+    }
 
 })();
