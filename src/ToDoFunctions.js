f774a90f10ba08b885927d71609e66fb791c19af
sracabanahashas <wkocsis@gmail.com>
2024-09-30 09:03:42 -0700

    Add comment descriptions of each function, begin to refactor project creation process to its own set of functions

--- src/ToDoFunctions.js
+++ src/ToDoFunctions.js
@@ -1,15 +1,17 @@
 export const test = "Sup";
 
-//functions: create, edit, delete todo items; create, read, edit projects
+//functions: create, edit, delete To Do items; create, read, edit projects
 export const ToDoController = (function () {
 
     let currentToDoItem;
     let projects = [];
     
+    // return the current To Do item
     function getToDoItem() {
         return currentToDoItem;
     }
 
+    // create a new To Do item object
     function createToDoItem(title, description, dueDate, priority, project) {
         return {
             title,
@@ -20,18 +22,27 @@ export const ToDoController = (function () {
         }
     }
 
+    // add a new To Do item object inside the project to which it's assigned and set the object as the currentToDoItem
     function addToDoItem(title, description, dueDate, priority, project) {
         const newToDoItem = createToDoItem(title, description, dueDate, priority, project)
         console.log(newToDoItem.title);
         console.log(newToDoItem.project);
+
         const newProject = [];
         projects.push(newProject);
         newProject.push(newToDoItem);
         console.log(projects);
+
         currentToDoItem = newToDoItem;
     }
 
-    
+    function createProject(toDoItem, projectName) {
+        const projectName = 
+    }
+
+    function addProject(projectName) {
+
+    }
 
     return {
         createToDoItem,
@@ -43,7 +54,7 @@ export const ToDoController = (function () {
 
 export const DomController = (function () {
 
-
+    // render the current To Do item to the DOM
     function renderToDoItem(currentToDoItem) {
         currentToDoItem = ToDoController.getToDoItem();
         console.log(currentToDoItem);
@@ -74,7 +85,8 @@ export const DomController = (function () {
         card.appendChild(project);
         project.textContent = currentToDoItem.project;
     }
-
+    
+    // add modal display, form functionality, creation and rendering to "Create New To-Do Item" button
     function createNewItemBtn() {
         const createBtn = document.querySelector('.createBtn');
         const modal = document.querySelector('.modal');
